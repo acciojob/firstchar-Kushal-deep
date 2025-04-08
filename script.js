a7027cfba18beb622f312f596a8ba240311ad3e0
Kushal-deep <kushaldeep448@gmail.com>
2025-04-08 14:56:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function firstChar(text) {
-  // your code here
-	let flag=0;
-	for(i=0;i<text.length;i++){
-		if(text[i]!=" "){
-			flag=1;
-			return text[i]
-		}
-	}
-	if(flag===0){
-		return " "
-	}
+  // Trim the string to remove leading and trailing spaces
+  const trimmedText = text.trim();
+
+  // Check if the trimmed string is empty
+  if (trimmedText.length === 0) {
+    return ""; // Return an empty string
+  }
+
+  // Return the first character of the trimmed string
+  return trimmedText[0]; 
 }
-  
+
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
+// Uncomment the following line to show the prompt popup
 const text = prompt("Enter text:");
 alert(firstChar(text));
