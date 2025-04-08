f51bb0f75f622c9e2e1d38c61e74c26d6de29180
Kushal-deep <kushaldeep448@gmail.com>
2025-04-08 14:49:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function firstChar(text) {
   // your code here
+	let flag=0;
 	for(i=0;i<text.length;i++){
 		if(text[i]!=" "){
+			flag=1;
 			return text[i]
 		}
 	}
+	if(flag===0){
+		return " "
+	}
 }
-
+  
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
 const text = prompt("Enter text:");
